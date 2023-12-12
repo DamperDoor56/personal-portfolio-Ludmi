717ade39ac1d1ab00c886d55e4226981132bb631
Ludmi <ludmit@outlook.com>
2023-12-11 21:25:22 -0300

    feat: :sparkles: started presentation page

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
 import { Navbar } from '@/components/Navbar'
+import { Presentation } from '@/pages/Presentation'
 import Image from 'next/image'
 
 export default function Home() {
   return (
     <main>
       <Navbar />
+      <Presentation />
     </main>
   )
 }
---
+++ pages/Presentation.tsx
@@ -0,0 +1,16 @@
+export function Presentation() {
+    return (
+        <article className="bg-background-gray h-[100vh] w-full flex justify-center items-start">
+            <div className="font-raleway flex flex-col w-full justify-center pt-[10rem] px-[22rem]">
+            <p className="font-light self-start text-2xl">Hello there!</p>
+            <div className="font-semibold self-center text-8xl">
+            <h1>
+            I&apos;m <span>Ludmila</span>, a</h1>
+            <span className="pl-20">Front-End Developer</span>
+            </div>
+            <p className="font-light self-end text-2xl">Nice to meet you!</p>
+            </div>
+
+        </article>
+    )
+}
