9c1d49f59479701b9604434d2904f6545680fea7
Ludmi <ludmit@outlook.com>
2023-12-14 09:58:01 -0300

    feat: :sparkles: add typewriter to main view

--- pages/Presentation.tsx
+++ pages/Presentation.tsx
@@ -1,16 +1,19 @@
+import Typewriter from "@/components/Typewriter";
+
 export function Presentation() {
     return (
-        <article className="bg-background-gray h-[100vh] w-full flex justify-center items-start">
+        <article className="bg-background-gray h-[90vh] w-full flex justify-center items-start">
             <div className="font-raleway flex flex-col w-full justify-center pt-[10rem] px-[22rem]">
             <p className="font-light self-start text-2xl">Hello there!</p>
-            <div className="font-semibold self-center text-8xl">
-            <h1>
+            <div className="font-semibold self-center text-8xl w-full">
+            <h1 className="whitespace-nowrap pl-20">
             I&apos;m <span>Ludmila</span>, a</h1>
-            <span className="pl-20">Front-End Developer</span>
+            <Typewriter className="pl-40 text-aqua-green whitespace-nowrap"
+            texts={["Front-End Developer", "Back-End Developer", "Software Developer"]}
+            delay={200} />
             </div>
             <p className="font-light self-end text-2xl">Nice to meet you!</p>
             </div>
-
         </article>
     )
 }
