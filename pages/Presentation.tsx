ee5c263032faf9a43325e8cd18ef92ba5c74a8d8
Ludmi <ludmit@outlook.com>
2024-01-19 16:33:18 -0300

    style: :art: updated and corrected styles in presentation

--- pages/Presentation.tsx
+++ pages/Presentation.tsx
@@ -4,16 +4,16 @@ export function Presentation() {
 
       return (
         <article className="bg-background-gray h-[90vh] w-full flex justify-center items-start">
-            <div className="font-raleway flex flex-col w-full justify-center pt-[10rem] px-[22rem]">
+            <div className="font-raleway grid w-full justify-center pt-[10rem] px-[22rem]">
             <p className="font-light self-start text-2xl">Hello there!</p>
             <div className="font-semibold self-center text-8xl w-full">
             <h1 className="whitespace-nowrap pl-20">
             I&apos;m <span className="text-lily">Ludmila</span>, a</h1>
               <TypeWriter delay={2000} 
-              texts={['Software Developer', 'Back-End Developer','Front-End Developer']}
+              texts={['Software Developer','Back-End Developer','Front-End Developer']}
               className="pl-40 text-aqua-green whitespace-nowrap text-8xl"/>
             </div>
-            <p className="font-light self-end text-2xl">Nice to meet you!</p>
+            <p className="font-light justify-self-end text-2xl">Nice to meet you!</p>
             </div>
         </article>
     )
