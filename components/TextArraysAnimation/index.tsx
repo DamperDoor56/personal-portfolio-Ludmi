b46da816a123d773bbbfaef35bdd7c5e2bcf558e
Ludmi <ludmit@outlook.com>
2024-01-19 17:20:59 -0300

    feat: :sparkles: changed names

--- components/TextArraysAnimation/index.tsx
+++ components/TextArraysAnimation/index.tsx
@@ -4,16 +4,19 @@ import classNames from 'classnames';
 import React, { useState, useEffect } from 'react';
 import { TextArraysAnimationProps } from './types';
 
-
 const TextArraysAnimation: React.FC<TextArraysAnimationProps> = ({ texts, className, delay }) => {
+  // Keep track of the array index
   const [currentArrayIndex, setCurrentArrayIndex] = useState<number>(0);
+  // Decide if we need the animation
   const [shouldAnimate, setShouldAnimate] = useState<boolean>(false);
 
   useEffect(() => {
+    // Trigger the animation when the component mounts or the text changes
     setShouldAnimate(true);
 
     const timer = setTimeout(() => {
       setCurrentArrayIndex((prevIndex) => (prevIndex + 1) % texts.length);
+      // after the text end, cancel the animation
       setShouldAnimate(false);
     }, delay);
 
@@ -21,6 +24,7 @@ const TextArraysAnimation: React.FC<TextArraysAnimationProps> = ({ texts, classN
 
   }, [currentArrayIndex, delay, texts]);
 
+  // animation
   const fadeInOutClasses = classNames(
     'opacity-0',
     'transition-opacity',
@@ -30,7 +34,7 @@ const TextArraysAnimation: React.FC<TextArraysAnimationProps> = ({ texts, classN
   );
 
   return (
-        <p key={currentArrayIndex} className={classNames(fadeInOutClasses, className)}>
+        <p key={currentArrayIndex} className={classNames(fadeInOutClasses, className, 'min-w-[75rem]')}>
           {texts[currentArrayIndex]}
         </p>
   );
