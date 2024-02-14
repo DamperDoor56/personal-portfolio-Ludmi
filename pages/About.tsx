356e5f57b33ce4cfaf91ede0b552d89b850c5454
Ludmi <ludmit@outlook.com>
2024-02-14 17:29:33 -0300

    style: :art: finished layout, about section, needs responsiveness

---
+++ pages/About.tsx
@@ -0,0 +1,28 @@
+import Image from "next/image";
+import Circle from "@/public/assets/Circle-light-blue.png";
+import GrayCircle from "@/public/assets/gray-big-circle.png";
+
+export function About() {
+      return (
+        <section className="bg-background-gray h-[80vh] font-gothic w-full gap-14 flex flex-col items-center">
+            <div className="grid gap-5 text-center z-10">
+            <h1 className="text-7xl">ABOUT ME</h1>
+            <hr className="w-80 h-1 bg-aqua-green border-aqua-green" />
+            </div>
+                <p className="text-2xl w-[45rem] self-center leading-10 z-10">
+                My name is Ludmila, and I&apos;m a Software Developer with a <span className="text-lily">diverse background in building innovative products and projects</span>. <br/>
+                I&apos;ve contributed to both front-end and back-end development, collaborating with agile teams in various work environments, ranging from large to small teams.
+                Additionally, I&apos;m comfortable working independently.  <br/>
+                Throughout my development journey, I&apos;ve acquired skills <span className="text-lily">in design patterns</span>, used Git for version control, and managed projects with tools like Jira. 
+                <span className="text-lily"> I have experience with content management systems (CMS) and a strong foundation in testing methodologies.</span>
+                I have experience in a variety of technologies, including <span className="text-lily"> NextJS, Strapi, Firebase, MongoDB, ExpressJS, SQL, MySQL, Redux, React Native, SASS, Tailwind, Bootstrap, Laravel/PHP, and Node.js/ Nest.js.</span>
+                </p>
+            <aside className="absolute right-0 z-0">
+                <Image src={Circle} alt={"circle-light-blue"} />
+            </aside>
+            <aside className="absolute left-0 z-0">
+                <Image src={GrayCircle} alt={"circle-light-blue"} />
+            </aside>
+        </section>
+    )
+}
